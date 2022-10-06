daa6f694953e7495e57ca499c52bc7ebb7155b56
Ollly19 <79220370+ollllya19@users.noreply.github.com>
2022-10-06 14:52:54 +0500

    admin panel customisation

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,4 @@
+
 from django.contrib import admin
 
 from .models import UserProfile, Project, Task, UserProjectTask
@@ -6,10 +7,25 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'name', 'image', 'joined_date')
     list_display_links = ('id',)
     search_fields = ('user', 'name')
-    list_editable = ('name',)
-    list_filter = ('user', 'name', 'joined_date')
     
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'creator', 'member_count', 'created_at')
+    list_display_links = ('id',)
+    search_fields = ('title',)
+    list_filter = ('title', 'created_at')
+    
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'creator', 'description', 'is_done', 'created_date')
+    list_display_links = ('id',)
+    search_fields = ('title',)
+    list_filter = ('title', 'creator', 'created_date')
+
+class UserProfileTaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'project', 'task')
+    list_display_links = ('id',)
+    list_filter = ('user', 'project', 'task')
+
 admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(Project)
-admin.site.register(Task)
-admin.site.register(UserProjectTask)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(UserProjectTask, UserProfileTaskAdmin)
